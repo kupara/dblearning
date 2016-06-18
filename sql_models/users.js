4a9ec1ca7722539b78daf08dcf822ba0892b3d88
andela-ekupara <edwin.kupara@andela.com>
2016-06-18 10:05:37 +0300

    Complete user model

--- sql_models/users.js
+++ sql_models/users.js
@@ -1,5 +1,30 @@
 'use strict';
 
 module.exports = function (sequelize, DataTypes) {
+  var User =  sequelize.define('User', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING
+      }
+    }, {
+      underscored: true,
 
+    }
+  });
 }
